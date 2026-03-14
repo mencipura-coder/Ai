af30f322a96f91a281260e00e8684f0ad027f05b
wakatobi-update <mencipura@gmail.com>
2026-03-15 02:25:18 +0900

    Update post.js

--- js/post.js
+++ js/post.js
@@ -20,9 +20,8 @@ behavior:"smooth"
 
 }
 
+
 function postingStatus(){
-console.log("posting jalan")
-}
 
 let text = document.getElementById("postText").value
 let image = document.getElementById("postImage").files[0]
@@ -73,51 +72,6 @@ savePosts()
 
 document.getElementById("postText").value = ""
 document.getElementById("postImage").value = ""
-
-alert("Posting berhasil!")
-
-  }
-
-let text = document.getElementById("postText").value
-
-if(text === ""){
-alert("Tulis sesuatu dulu")
-return
-}
-
-let feed = document.querySelector(".feed")
-
-let post = document.createElement("div")
-
-post.className = "product-card"
-
-post.innerHTML = `
-
-<div class="product-info">
-
-<h3>${text}</h3>
-
-<p class="location">📍 Sekitar kamu • ${getTime()}</p>
-
-<div class="product-action">
-
-<button class="chat">💬 Chat</button>
-
-<button class="map">📍 Map</button>
-
-<button class="love">❤</button>
-
-</div>
-
-</div>
-
-`
-
-feed.prepend(post)
-  
-savePosts()
-
-document.getElementById("postText").value = ""
 
 alert("Posting berhasil!")
 
